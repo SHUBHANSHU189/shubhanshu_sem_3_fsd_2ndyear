14d7c6daa008052c438f318e1b05fd33be6d3966
Shubhanshu Tiwari <shubhanshu.25b15410131@abes.ac.in>
2026-09-12 15:22:22 +0530

    datatypes

---
+++ lecture2/datatypes.js
@@ -0,0 +1,33 @@
+let a=5;
+let b=7.5;
+let c="shubhanshu";
+let d=true;
+let e;
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+
+
+let arr=["he","she","it"];
+console.log(typeof(arr));
+
+document.write("hello everyone");
+let obj={name:"shubh",class:"dsa"};
+console.log(typeof(obj));
+
+let myfunction = function(){
+    document.write("hello everyone");
+}
+
+console.log(typeof(myfunction));
+
+alert(
+    "hello world"
+);
+
+confirm(
+    "do ypu want to submit your number"
+);
---
+++ lecture2/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let a=5;
+let b=7.5;
+let c="shubhanshu";
+
+console.log(typeof(a));
+console.log(typeof(b);
+console.log(typeof(c));
