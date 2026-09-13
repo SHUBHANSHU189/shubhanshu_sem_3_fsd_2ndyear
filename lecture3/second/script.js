424b5b03ab001dd7cc51661e6a3b73fd6672e811
Shubhanshu Tiwari <shubhanshu.25b15410131@abes.ac.in>
2026-09-13 21:30:23 +0530

    day3

---
+++ lecture3/second/script.js
@@ -0,0 +1,7 @@
+function multiply(a,b){
+    return a*b;
+}
+let num1= Number(prompt("enter the first number"));
+let num2=Number(prompt("enter the second number"));
+let result= multiply(num1,num2);
+console.log(("the result is:"+result+"<br>"));
